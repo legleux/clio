4f983f084627274d4ccf6c8ee8f877c009cd4331
Michael Legleux <mlegleux@ripple.com>
2022-08-10 12:54:00 -0700

    fix install_cmake.sh

--- docker/install_cmake.sh
+++ docker/install_cmake.sh
@@ -1,13 +1,11 @@
+#!/bin/bash
 
-CMAKE_VERSION=$1
-# IFS=. read -r MAJ MIN <<< $CMAKE_FULL_VERSION
-# MAJ=$(echo $CMAKE_FULL_VERSION | tr "." " " | cut -d ' ' -f 1)
-# MIN=$(echo $CMAKE_FULL_VERSION | tr "." " " | cut -d ' ' -f 2)
-# P=$(echo $CMAKE_FULL_VERSION | tr "." " " | cut -d ' ' -f 3)
-# CMAKE_VERSION=$MAJ.$MIN
+set -eo pipefail
+
+CMAKE_VERSION=${1:-"3.16.3"}
+cd /tmp
 URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz"
 curl -OJLs $URL
 tar xzvf cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz
-ln -s /cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake /usr/local/bin/cmake
-
-# https://github.com/Kitware/CMake/releases/download/v3.16.3/cmake-3.16.3-Linux-x86_64.tar.gz
+mv cmake-${CMAKE_VERSION}-Linux-x86_64 /opt/
+ln -s /opt/cmake-${CMAKE_VERSION}-Linux-x86_64/bin/cmake /usr/local/bin/cmake
