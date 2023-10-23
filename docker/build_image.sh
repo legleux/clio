1ad3918bcb228c82130452349a3d0b357a00e8c2
Michael Legleux <mlegleux@ripple.com>
2023-10-23 13:32:28 -0700

    f

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,21 +2,28 @@
 set -ex
 
 REF=$(git rev-parse --short HEAD)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+RUN_DIR=$(basename $PWD)
 
-if [[ $(basename $PWD) == 'docker' ]]; then
+echo "Script lives in $SCRIPT_DIR"
+echo "Script run from: $PWD"
+echo "Script run from dirname: $RUN_DIR"
+
+if [[ ${RUN_DIR} == "docker" ]]; then
     cd ..
 fi
 
+
+echo "CLIO_GIT_REF=$REF" > $SCRIPT_DIR/.env
 # build the clio builder image
 #docker build . --target builder --tag clio-builder:$REF  --file docker/Dockerfile
 
 # build clio
-docker build . --target build_clio --file docker/Dockerfile
+#docker build . --target build_clio --file docker/Dockerfile
 
 # build the clio server image
-docker build .  --target clio --tag clio:$REF --file docker/Dockerfile
+#docker build .  --target clio --tag clio:$REF --file docker/Dockerfile
 
-echo "CLIO_GIT_REF=$REF" > .env
 # tag it with the git tag if we're on a tagged commit
 # git describe --exact-match --tags $(git log -n1 --pretty='%h')
 # or
