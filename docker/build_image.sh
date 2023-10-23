4e9081c2cda929b9fa00c07a4b7dfa4b03c50ced
Michael Legleux <mlegleux@ripple.com>
2023-10-23 10:50:46 -0700

    f

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -3,12 +3,20 @@ set -ex
 
 REF=$(git rev-parse --short HEAD)
 
+if [[ $(basename $PWD) == 'docker' ]]; then
+    cd ..
+fi
+
 # build the clio builder image
-docker build . --target builder --tag clio-builder:10-17  --file docker/Dockerfile
+#docker build . --target builder --tag clio-builder:$REF  --file docker/Dockerfile
+
+# build clio
+docker build . --target build_clio --file docker/Dockerfile
 
 # build the clio server image
 docker build .  --target clio --tag clio:$REF --file docker/Dockerfile
 
+echo "CLIO_GIT_REF=$REF" > .env
 # tag it with the git tag if we're on a tagged commit
 # git describe --exact-match --tags $(git log -n1 --pretty='%h')
 # or
