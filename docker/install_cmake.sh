1b4032b37c0d1dcd0d0d5e28e856b579b3a1974c
Michael Legleux <mlegleux@ripple.com>
2023-10-23 18:01:04 -0700

    Docker compose and docker image upload
    rm old docker stuff

---
+++ docker/install_cmake.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -ex
+IFS='.' read -ra CMAKE_VERSION_ARRAY  <<< $CMAKE_VERSION
+read -r CMAKE_MAJ_VERSION CMAKE_MIN_VERSION CMAKE_PATCH_VERSIONc <<< ${CMAKE_VERSION_ARRAY[@]}
+CMAKE_SCRIPT="cmake-${CMAKE_VERSION}-linux-x86_64.sh"
+
+curl -OJL "https://cmake.org/files/v${CMAKE_MAJ_VERSION}.${CMAKE_MIN_VERSION}/${CMAKE_SCRIPT}"
+chmod +x "${CMAKE_SCRIPT}"
+
+./"${CMAKE_SCRIPT}" --skip-license --prefix=/usr/local
+rm "${CMAKE_SCRIPT}"
