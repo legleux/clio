9f63b7572c9c842d453d2bb23ead1760a008bdf6
Michael Legleux <mlegleux@ripple.com>
2023-10-25 15:43:34 -0700

    build image

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -5,6 +5,7 @@ REF=$(git rev-parse --short HEAD)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RUN_DIR=$(basename $PWD)
 
+IMAGE_NAME=${1:-clio\:$REF}
 echo "Script lives in $SCRIPT_DIR"
 echo "Script run from: $PWD"
 echo "Script run from dirname: $RUN_DIR"
@@ -15,16 +16,16 @@ fi
 
 
 echo "CLIO_GIT_REF=$REF" > $SCRIPT_DIR/.env
-# build the clio builder image
+# # build the clio builder image
 #docker build . --target builder --tag clio-builder:$REF  --file docker/Dockerfile
 
-# build clio
+# # build clio
 #docker build . --target build_clio --file docker/Dockerfile
 
-# build the clio server image
-#docker build .  --target clio --tag clio:$REF --file docker/Dockerfile
+# # build the clio server image
+docker build .  --target clio --tag "${IMAGE_NAME}" --file docker/Dockerfile
 
-# tag it with the git tag if we're on a tagged commit
+# # tag it with the git tag if we're on a tagged commit
 # git describe --exact-match --tags $(git log -n1 --pretty='%h')
-# or
+# # or maybe
 # git describe --tags # <-- returns last tag found... not quite what we want
