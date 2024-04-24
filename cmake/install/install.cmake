f15f370b0e2b1074299d31ebe2dbdbee5454a1df
Michael Legleux <mlegleux@ripple.com>
2024-04-23 22:58:06 -0700

    update install

--- cmake/install/install.cmake
+++ cmake/install/install.cmake
@@ -1,13 +1,13 @@
-set(CLIO_INSTALL_DIR "/opt/clio")
-set(CMAKE_INSTALL_PREFIX ${CLIO_INSTALL_DIR})
+#set(CLIO_INSTALL_DIR "/opt/clio")
+#set(CMAKE_INSTALL_PREFIX ${CLIO_INSTALL_DIR})
 
-install(TARGETS clio_server DESTINATION bin)
+install(TARGETS clio_server RUNTIME)
 
-file(READ docs/examples/config/example-config.json config)
-string(REGEX REPLACE "./clio_log" "/var/log/clio/" config "${config}")
-file(WRITE ${CMAKE_BINARY_DIR}/install-config.json "${config}")
-install(FILES ${CMAKE_BINARY_DIR}/install-config.json DESTINATION etc RENAME config.json)
+# file(READ docs/examples/config/example-config.json config)
+# string(REGEX REPLACE "./clio_log" "/var/log/clio/" config "${config}")
+# file(WRITE ${CMAKE_BINARY_DIR}/install-config.json "${config}")
+# install(FILES ${CMAKE_BINARY_DIR}/install-config.json DESTINATION etc RENAME config.json)
 
-configure_file("${CMAKE_SOURCE_DIR}/cmake/install/clio.service.in" "${CMAKE_BINARY_DIR}/clio.service")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/clio.service.in" "${CMAKE_BINARY_DIR}/clio.service")
 
 install(FILES "${CMAKE_BINARY_DIR}/clio.service" DESTINATION /lib/systemd/system)
